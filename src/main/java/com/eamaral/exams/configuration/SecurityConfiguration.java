9abdb1ed6288ac99248b377f382da49e2a1a15d7
EduardoAmaral <eduardofsamaral@gmail.com>
2020-03-14 17:27:39 -0300

    Disable crsf

--- src/main/java/com/eamaral/exams/configuration/SecurityConfiguration.java
+++ src/main/java/com/eamaral/exams/configuration/SecurityConfiguration.java
@@ -14,6 +14,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http.authorizeRequests()
                 .antMatchers("/api/*")
                 .authenticated()
