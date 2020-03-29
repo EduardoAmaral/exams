2466ec9b2c31277b80a4f95219cec1301a3a5ec1
EduardoAmaral <eduardofsamaral@gmail.com>
2020-03-29 20:07:44 -0300

    Add code lint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-    return (
-        <div className="App">
-            Hello Exams!
-        </div>
-    );
+  return (
+    <div className="App">
+      Hello Exams!
+    </div>
+  );
 }
 
 export default App;
