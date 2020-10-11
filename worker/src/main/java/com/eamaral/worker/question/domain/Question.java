176bb7a4ff1448ca6bbea7df74af580bc059a5b1
EduardoAmaral <eduardofsamaral@gmail.com>
2020-10-11 20:35:05 -0300

    [Refactoring] Fix unnecessary equals and hash code from worker

--- worker/src/main/java/com/eamaral/worker/question/domain/Question.java
+++ worker/src/main/java/com/eamaral/worker/question/domain/Question.java
@@ -2,12 +2,10 @@ package com.eamaral.worker.question.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @Builder(toBuilder = true)
-@EqualsAndHashCode
 @AllArgsConstructor
 public class Question {
 
